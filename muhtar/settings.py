6212633a326628827ec76823fcbf3848d3e356c0
Cihad GÜNDOĞDU <cihadgundogdu@gmail.com>
2024-10-07 16:43:58 +0300

    Ufak tefek

--- muhtar/settings.py
+++ muhtar/settings.py
@@ -15,7 +15,9 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
+INTERNAL_IPS = [
+    "127.0.0.1",
+]
 # Application definition
 
 INSTALLED_APPS = [
@@ -25,6 +27,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "debug_toolbar",
     "account.apps.AccountConfig",
     "gelir.apps.GelirConfig",
 ]
@@ -37,6 +40,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "debug_toolbar.middleware.DebugToolbarMiddleware",
 ]
 
 ROOT_URLCONF = "muhtar.urls"
